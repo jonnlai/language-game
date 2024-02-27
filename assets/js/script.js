c9a78b6cb2291560af5db337b8cb7abe0f1cb8b9
jonnlai <121501241+jonnlai@users.noreply.github.com>
2024-02-27 20:06:24 +0000

    feat: create function to drag and drop options in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,6 +32,8 @@ window.onload = function () {
   for (let optionRow = 0; optionRow < optionRows; optionRow++) {
     let option = document.createElement("div");
     option.id = "option-" + optionRow.toString();
+    // How to make an element draggable was taken from Darwin Tech JavaScript Drag and Drop Youtube tutorial: https://www.youtube.com/watch?v=_G8G1OrEOrI
+    option.draggable = "true";
     //option.textContent = option.id;
     optionsArea.append(option);
   }
@@ -50,20 +52,56 @@ function selectGame() {
 }
 
 /**
+ * Allow the user to drag and drop the option next to the correct word
+ * How to use drag and drop event listeners was taken from: https://github.com/ImKennyYip/slide-puzzle/blob/master/puzzle.js
+ * and https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/drag_event
+ * and Darwin Tech JavaScript Drag and Drop Youtube tutorial: https://www.youtube.com/watch?v=_G8G1OrEOrI
+ */
+function playGame() {
+  let selectedOption;
+
+  // Add "drop-zone" class to all div elements with an id starting with "1-"
+  // How to convert NodeList to Array taken from https://www.geeksforgeeks.org/fastest-way-to-convert-javascript-nodelist-to-array/
+  let dropZone = Array.from(document.querySelectorAll("[id^='1-'"));
+  for (let x in dropZone) {
+    dropZone[x].classList.add("drop-zone");
+    dropZone[x].addEventListener("dragover", function (event) {
+      event.preventDefault();
+    });
+    dropZone[x].addEventListener("drop", function (event) {
+      dropZone[x].prepend(selectedOption);
+    });
+  }
+
+  let optionWords = Array.from(document.querySelectorAll("[id^='option-'"));
+  for (let x in optionWords) {
+    optionWords[x].addEventListener("dragstart", function () {
+      selectedOption = this;
+    });
+  }
+
+  let optionsArea = document.getElementById("options-area");
+  optionsArea.addEventListener("dragover", function (event) {
+    event.preventDefault();
+  });
+  optionsArea.addEventListener("drop", function () {
+    optionsArea.prepend(selectedOption);
+  });
+}
+
+/**
  * Display English to Finnish animal names game
  */
 function displayEngFinAnimals() {
   // How to use Javascript wildcard was taken from: https://www.js-craft.io/blog/javascript-use-queryselectorall-with-wildcards/
-  //Select the div that's id starts with "0-"
+  //Select all the divs that have an id that starts with "0-"
   let gameWords = document.querySelectorAll("[id^='0-'");
-  console.log(gameWords);
   let animalNames = Object.values(animals);
-  console.log(animalNames);
+  //Select all the divs that have an id that starts with "option-"
   let optionWords = document.querySelectorAll("[id^='option-'");
-  console.log(optionWords);
 
+  // Add the English words in the game area
   let x = 0;
-
   for (let y in gameWords) {
     gameWords[y].textContent = animalNames[x][0];
     if (x < 7) {
@@ -71,23 +109,14 @@ function displayEngFinAnimals() {
     }
   }
 
+  // Add the translated Finnish words in the options area
   let b = 0;
   for (let a in optionWords) {
-    console.log(optionWords[a]);
     optionWords[a].textContent = animalNames[b][1];
     if (b < 5) {
       b += 1;
     }
   }
-  // let originalName = document.getElementById("0-0").textContent;
-  // console.log(document.getElementById("0-0").textContent);
-  // originalName.textContent = animals.dog[0];
-  // let translatedName = document.getElementById("option0");
-  // translatedName.textContent = animals.dog[1];
+
+  playGame();
 }
-//   for (animal in animals) {
-//     let engName = document.getElementById("0-0");
-//     engName.innerText = animals.dog[0];
-//     console.log(animal);
-//   }
-// }
